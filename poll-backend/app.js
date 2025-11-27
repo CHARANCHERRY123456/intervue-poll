9325e86ea0ed6f04bda772f52068afd437e6b0d0
CHARANCHERRY123456 <cherryiiit1234@gmail.com>
2025-11-27 10:26:09 +0530

    inital commit with setup frotnend backend

---
+++ poll-backend/app.js
@@ -0,0 +1,24 @@
+import express from "express"
+import cors from "cors"
+import helmet from "helmet"
+import morgan from "morgan"
+import { rateLimiter } from "./config/rateLimit.js"
+import { corsOptions } from "./config/cors.js"
+// import router from "./routes/index.js"
+// import notFound from "./middleware/notFound.js"
+// import errorHandler from "./middleware/errorHandler.js"
+
+const app = express()
+
+app.use(cors(corsOptions))
+app.use(helmet())
+app.use(morgan("dev"))
+app.use(rateLimiter)
+app.use(express.json())
+
+// app.use("/api", router)
+
+// app.use(notFound)
+// app.use(errorHandler)
+
+export default app;
