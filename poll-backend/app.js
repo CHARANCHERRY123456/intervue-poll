4ac91bdeb56d37df62cbfa35c8a0bb6299aedd2d
CHARANCHERRY123456 <cherryiiit1234@gmail.com>
2025-11-29 10:38:18 +0530

    fix: remove custom CORS options for default configuration in app.js

--- poll-backend/app.js
+++ poll-backend/app.js
@@ -3,14 +3,13 @@ import cors from "cors"
 import helmet from "helmet"
 import morgan from "morgan"
 import { rateLimiter } from "./config/rateLimit.js"
-import { corsOptions } from "./config/cors.js"
 import notFound from "./middleware/notFound.js"
 import errorHandler from "./middleware/errorHandler.js"
 import router from "./routes/poll.routes.js"
 
 const app = express()
 
-app.use(cors(corsOptions))
+app.use(cors()); 
 app.use(helmet())
 app.use(morgan("dev"))
 app.use(rateLimiter)
